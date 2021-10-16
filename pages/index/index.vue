1c1207d722218c124b27a8069709dd0725be1d4c
752235 <15800349672@163.com>
2021-10-16 14:30:17 +0800

    first commit

---
+++ pages/index/index.vue
@@ -0,0 +1,32 @@
+<template>
+	<view class="content">
+		<button type="primary" size="mini" @click="goto('/pages/about/about')">通过方法跳转到about页面</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			goto(url) {
+				uni.navigateTo({
+					url:"../player/index"
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		align-items: center;
+		padding-top: 100rpx;
+	}
+</style>
---
+++ pages/player/index.vue
@@ -0,0 +1,20 @@
+<template>
+	<view>
+		<web-view class="webview-container" src="/hybrid/html/player.html"></web-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		}
+	}
+</script>
+
+<style>
+	.webview-container {
+		/* border: 1px #f00 solid; */
+	}
+</style>
